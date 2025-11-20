d00dfdbc9dcab4880e64495ba1afdaac84e38c17
Ravish <rkumarsinha.sinha500@gmail.com>
2025-11-20 15:04:18 +0530

    committed files

---
+++ wp-content/plugins/essential-blocks/assets/admin/quick-setup/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '03a521f91e5984ada7eb');
---
+++ wp-content/plugins/essential-blocks/assets/blocks/slider/frontend.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-dom-ready', 'wp-element'), 'version' => 'b3ce3f7cfbc64daf8732');
